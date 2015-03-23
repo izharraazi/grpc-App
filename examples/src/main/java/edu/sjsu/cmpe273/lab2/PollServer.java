9a5e2014194e529d514786aefebd96afd001a963
izharraazi <izharraazi@gmail.com>
2015-03-22 20:28:54 -0700

    removed sysout to add logger.info

--- examples/src/main/java/edu/sjsu/cmpe273/lab2/PollServer.java
+++ examples/src/main/java/edu/sjsu/cmpe273/lab2/PollServer.java
@@ -1,34 +1,4 @@
-/*
- * Copyright 2015, Google Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *    * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *    * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *
- *    * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
+//Izhar Raazi 
 package edu.sjsu.cmpe273.lab2;
 
 import io.grpc.ServerImpl;
@@ -83,7 +53,7 @@ public class PollServer{
    public void createPoll(edu.sjsu.cmpe273.lab2.PollRequest request,
         io.grpc.stub.StreamObserver<edu.sjsu.cmpe273.lab2.PollResponse> responseObserver){
 		PollResponse reply = PollResponse.newBuilder().setId("123456").build();
-		 System.out.println("The Moderator Id for which the poll is created is ::"+request.getModeratorId());
+		 logger.info("The Moderator Id for which the poll is created is ::"+request.getModeratorId());
       responseObserver.onValue(reply);
       responseObserver.onCompleted();
 }
